ec1dc9a0304b1f952ff1ae9be7e7bddc5c522735
Chris McKenzie <kristopolous@yahoo.com>
2014-11-13 20:15:15 -0800

    updated minifier

--- minifier.sh
+++ minifier.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
+in=evda.js
+out=evda.min.js
 
-minify () {
-  curl -s \
-    -d compilation_level=SIMPLE_OPTIMIZATIONS \
-    -d output_format=text \
-    -d output_info=compiled_code \
-    --data-urlencode "js_code@${1}.js" \
-    http://closure-compiler.appspot.com/compile \
-    > "${1}.min.js"
-}
+before=`stat -c %s $out`
+beforeCompress=`gzip -c $out | wc -c`
+curl -s \
+        -d compilation_level=SIMPLE_OPTIMIZATIONS \
+        -d output_format=text \
+        -d output_info=compiled_code \
+        --data-urlencode "js_code@${in}" \
+        http://marijnhaverbeke.nl/uglifyjs \
+        > $out
+after=`stat -c %s $out`
+afterCompress=`gzip -c $out | wc -c`
 
-minify evda
+echo "raw: $before -> $after"
+echo "gzip: $beforeCompress -> $afterCompress"
