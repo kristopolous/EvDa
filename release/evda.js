22e024c7252ea0c973bf13e28c00a9bae8570874
Chris McKenzie <kristopolous@yahoo.com>
2016-04-06 21:02:42 -0700

    minified

--- release/evda.js
+++ release/evda.js
@@ -414,7 +414,13 @@ var
         // so that we can unregister it in the future.
         callback.$.ref.push( stage + key );
         // And so we can know where things were registered.
-        callback.$.line.push( (new Error).stack );
+        if(isString(callback.$.line)) {
+          callback.$.line = [callback.$.line];
+        }
+        callback.$.line.push ( (new Error).stack );
+        if(callback.$.line.length === 1) {
+          callback.$.line = callback.$.line[0];
+        }
 
         if (isGlobbed(key)) {
           my_map = globberMap;
@@ -1375,4 +1381,4 @@ var
 
   return e;
 })();
-EvDa.__version__='0.1-versioning-added-117-ga570d80';
+EvDa.__version__='0.1-versioning-added-119-g05f0eda';
