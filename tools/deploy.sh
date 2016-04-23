f9e112ace72b104f1607326c822a62e48d90e772
Chris McKenzie <kristopolous@yahoo.com>
2016-04-22 17:00:33 -0700

    New versioning mechanics with dates

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -3,7 +3,9 @@ for file in evda evda-helper; do
   cp src/$file.js release/$file.js
 done
 
-echo "EvDa.__version__='`git describe`';" >> release/evda.js
+version=`git describe | awk -F '-' ' { print $1"."$(NF-1) } '`
+age=`date +%Y%m%d`
+echo "EvDa.version='$version-$age';" >> release/evda.js
 
 cd release
 for file in evda evda-helper; do
